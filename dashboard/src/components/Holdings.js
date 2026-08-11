3f3605b0a1d76f3e53271217e1990f77979ae321
SoumyaGalkar <soumyagalkar@gmail.com>
2026-08-12 03:59:18 +0530

    Clean up Holdings.js by removing unused code
    Removed commented-out dataset code from Holdings.js.

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+// eslint-disable-next-line 
 import axios, { all } from "axios";
 import { VerticalGraph } from "./VerticalGraph";
 
@@ -26,21 +27,6 @@ const Holdings = () => {
     ],
   };
 
-  // export const data = {
-  //   labels,
-  //   datasets: [
-  // {
-  //   label: 'Dataset 1',
-  //   data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-  //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
-  // },
-  //     {
-  //       label: 'Dataset 2',
-  //       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-  //       backgroundColor: 'rgba(53, 162, 235, 0.5)',
-  //     },
-  //   ],
-  // };
 
   return (
     <>
