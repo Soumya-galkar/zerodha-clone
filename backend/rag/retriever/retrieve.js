1deea82508dc990a79b7a004af0dddcfb26cdf9a
SoumyaGalkar <soumyagalkar@gmail.com>
2026-08-13 03:44:30 +0530

    Create retrieve.js

---
+++ backend/rag/retriever/retrieve.js
@@ -0,0 +1,19 @@
+const { getEmbedding } = require("../embeddings/embedder");
+const { search } = require("../vectordb/faiss");
+
+async function retrieveRelevantChunks(question, k = 5) {
+
+    console.log("Creating question embedding...");
+
+    const queryEmbedding = await getEmbedding(question);
+
+    console.log("Searching FAISS...");
+
+    const results = search(queryEmbedding, k);
+
+    return results;
+}
+
+module.exports = {
+    retrieveRelevantChunks,
+};
