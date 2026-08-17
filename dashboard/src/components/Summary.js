c3a748e55d3e2aa53cc52952578e5e2d0d8b1860
SoumyaGalkar <soumyagalkar@gmail.com>
2026-08-18 03:35:10 +0530

    Update Summary.js

--- dashboard/src/components/Summary.js
+++ dashboard/src/components/Summary.js
@@ -47,7 +47,7 @@ const Summary = () => {
             console.log("Uploading:", selectedFile.name);
 
             const response = await fetch(
-                "https://zerodhabackend-igdj.onrender.com/rag/upload",
+                "https://zerodha-backend-bz7x.onrender.com/rag/upload",
                 {
                     method: "POST",
                     body: formData,
@@ -96,7 +96,7 @@ const Summary = () => {
             console.log("Question:", question);
 
             const response = await fetch(
-                "https://zerodhabackend-igdj.onrender.com/rag/chat",
+                "https://zerodha-backend-bz7x.onrender.com/rag/chat",
                 {
                     method: "POST",
                     headers: {
